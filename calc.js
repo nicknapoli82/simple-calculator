317eded344a9324afad29d6903858035c1657b85
Nicholas Napoli <nicknapoli82@gmail.com>
2019-04-07 20:43:27 -0700

    Fixed TIMES and DIVIDE as const values. Wasn't working before

--- calc.js
+++ calc.js
@@ -2,6 +2,8 @@
 
 const COLUMBS = 4;
 const ROWS = 5;
+const DIVIDE = '\xF7'; // HEX char code for divide
+const TIMES = '\xD7';  // HEX char code for times
 
 
 let main_container = document.getElementById("main_container");
@@ -48,8 +50,8 @@ function generate_button(element_attach, id, text_representation) {
     case ')':
     case '%':
     case 'AC':
-    case '&divide':
-    case '&times':
+    case DIVIDE:
+    case TIMES:
     case '-':
     case '+':
 	temp_element.style.backgroundColor = "rgb(195,195,195)";
@@ -81,8 +83,8 @@ function create_buttons_field() {
 		      ['one', 'two', 'three', 'subtract'],
 		      ['zero', 'dot', 'equals', 'addition']];
     let button_text = [['(', ')', '%', 'AC'],
-		       ['7', '8', '9', '&divide'],
-		       ['4', '5', '6', '&times'],
+		       ['7', '8', '9', DIVIDE],
+		       ['4', '5', '6', TIMES],
 		       ['1', '2', '3', '-'],
 		       ['0', '.', '=', '+']];
 
@@ -119,7 +121,6 @@ create_buttons_field();
 // based on some type of input
 function generate_result() {
     let equation = tokenize(text_field.innerText);
-
     let op_lh_rh = { op: 0, lh: 0, rh: 0 };
     let prescidence = 3;
     let equation_index = 0;
@@ -128,40 +129,37 @@ function generate_result() {
     // Probably should refactor this into a recursive function
     // Want to think a bit
     // Play with child :)
-    while (equation.length > 1) {
-	// Consume array bits by operator prescidence
-	if (prescidence === 3 && !Number(equation[equation_index])) {
+    // while (equation.length > 1) {
+    // 	// Consume array bits by operator prescidence
+    // 	if (prescidence === 3 && !Number(equation[equation_index])) {
 	    
-	}
-	else equation_index++;
-	if (prescidence === 2 && !Number(equation[equation_index])) {
+    // 	}
+    // 	else equation_index++;
+    // 	if (prescidence === 2 && !Number(equation[equation_index])) {
 	    
-	}
-	else equation_index++;
-	if (prescidence === 1 && !Number(equation[equation_index])) {
+    // 	}
+    // 	else equation_index++;
+    // 	if (prescidence === 1 && !Number(equation[equation_index])) {
 	    
-	}
-	else equation_index++;
-    }
+    // 	}
+    // 	else equation_index++;
+    // }
 }
 
 // tokenize takes raw text and returns an array of numbers and tokens
-const TOKENS = ['(', ')', '%', '&divide', '&times', '+', '-'];
+const TOKENS = ['(', ')', '%', DIVIDE, TIMES, '+', '-'];
 
 function tokenize(equation) {
     let result = [];
     let text_index = 0;
-
     while (text_index < equation.length) {
 	if (Number(equation[text_index])) {
 	    result.push(parseFloat(equation.slice(text_index)));
 	    text_index += (result[result.length - 1].toString()).length;
 	}
 
-	console.log(equation[text_index]);
-	
 	for (t in TOKENS) {
-	    if (TOKENS[t] === equation[text_index]) {
+	    if (TOKENS[t] == equation[text_index]) {
 		result.push(TOKENS[t]);
 		text_index++;
 		break;
